e296f4911e5f35cd443d5302a63d7ec9e08119a2
Mike Hennessy <michael.hennessy@dutchie.com>
2022-01-27 14:08:46 -0500

    feat: create config file in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,20 @@
 
 set -ueo pipefail
 
-# aws s3 cp ${S3_CERT_PEM} /etc/cloudflared/
+cat << EOF > /etc/cloudflared/config.yaml 
+tunnel: $CLOUDFLARED_TUNNEL_UUID
+credentials-file: /etc/cloudflared/$CLOUDFLARED_TUNNEL_UUID.json
+warp-routing:
+  enabled : true
+EOF
 
-# echo -e "url: ${ORIGIN_DNS}
-# hostname: ${TUNNEL_HOSTNAME}" > /etc/cloudflared/config.yml 
+echo $CLOUDFLARED_TUNNEL_CREDENTIALS > /etc/cloudflared/$CLOUDFLARED_TUNNEL_UUID.json 
 
 # set +ex
 
-# for i in {1..60}; do
-#   wget ${ORIGIN_DNS} 1>/dev/null 2>&1
+# for i in $(echo $TUNNELED_NETWORKS | awk 1 RS=,); do 
+#     echo $i; done
 
-#   if [ $? -ne 0 ]; then
-#     echo "Failed to connect to ${ORIGIN_DNS}"
-#     sleep 1
-#   else
-#     echo "Successfully connected to ${ORIGIN_DNS}"
-#     break
-#   fi
-# done
+# set -ex
 
-set -ex
-
-cloudflared tunnel --no-autoupdate
+cloudflared tunnel run
